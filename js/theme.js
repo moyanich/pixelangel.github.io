5640eab4fe6286ef37b9095a9364f9a4d5359ccd
pixelangel <ganymedenick@gmail.com>
2015-11-01 08:59:07 -0500

    update all files

--- js/theme.js
+++ js/theme.js
@@ -1,9 +1,10 @@
-$(document).foundation();
-
-$(function() {
-	$(".right-off-canvas-toggle, .exit-off-canvas").click(function() {
-    	$(".top-bar").toggleClass("top-bar-close");
-		$(".middle-bar").toggleClass("middle-bar-close");
-		$(".bottom-bar").toggleClass("bottom-bar-close");
-  	});
-});
+$(document).foundation({
+	"magellan-expedition": {
+		active_class: 'active', // specify the class used for active sections
+		threshold: 0, // how many pixels until the magellan bar sticks, 0 = auto
+		destination_threshold: 20, // pixels from the top of destination for it to be considered active
+		throttle_delay: 50, // calculation throttling to increase framerate
+		fixed_top: 0, // top distance in pixels assigend to the fixed element on scroll
+		offset_by_height: true // whether to offset the destination by the expedition height. Usually you want this to be true, unless your expedition is on the side.
+	}
+});	
